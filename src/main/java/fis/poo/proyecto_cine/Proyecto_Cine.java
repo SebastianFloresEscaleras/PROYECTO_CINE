e934c4a63de9aac3b08c456ef089cb17dd79f47f
sebas <sebas@penguin>
2026-07-07 08:15:50 -0500

    Primer commit

---
+++ src/main/java/fis/poo/proyecto_cine/Proyecto_Cine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fis.poo.proyecto_cine;
+
+/**
+ *
+ * @author sebas
+ */
+public class Proyecto_Cine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
