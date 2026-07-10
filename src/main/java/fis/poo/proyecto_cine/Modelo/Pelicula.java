eab2bd5f13f7c92e51ce3cb430268836e80451d5
coddex-tay <dayanna.guanga@epn.edu.ec>
2026-07-09 23:17:00 -0500

    modifique sala, pelicula y funcion

--- src/main/java/fis/poo/proyecto_cine/Modelo/Pelicula.java
+++ src/main/java/fis/poo/proyecto_cine/Modelo/Pelicula.java
@@ -18,7 +18,22 @@ public class Pelicula {
         this.minutos = minutos;
     }
     
-    
-    
+    //mi aporte (daya)
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
     
 }
